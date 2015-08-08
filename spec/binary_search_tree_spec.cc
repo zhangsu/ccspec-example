e8cba51cdb9e7417c8da8248471f7bba04a70b63
Su Zhang <me@zhang.su>
2015-08-07 22:14:58 -0500

    Add spec for #add

--- spec/binary_search_tree_spec.cc
+++ spec/binary_search_tree_spec.cc
@@ -7,14 +7,38 @@
 #include "binary_search_tree.h"
 
 using std::cout;
+using std::ostringstream;
 using ccspec::core::Reporter;
 using ccspec::core::describe;
 using ccspec::core::formatters::DocumentationFormatter;
 using ccspec::core::it;
 using ccspec::matchers::be;
+using ccspec::matchers::eq;
 using ccspec::expect;
 
 auto bst_spec = describe("BinarySearchTree", [] {
+  describe("#add", [] {
+    it("returns back the instance", [] {
+      BinarySearchTree bst;
+      BinarySearchTree& bst2 = bst.add(5);
+      expect(bst2).to(be(bst));
+    });
+
+    it("can build an arbitrary tree", [] {
+      BinarySearchTree bst;
+      bst.add(3).add(4).add(5).add(0).add(2).add(1);
+      ostringstream oss;
+      oss << bst;
+      expect(oss.str()).to(eq(
+          "    5\n"
+          "  4\n"
+          "3\n"
+          "    2\n"
+          "      1\n"
+          "  0\n"
+          ""));
+    });
+  });
 });
 
 int main() {

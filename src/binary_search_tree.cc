f598d18e3f17d8f8bdc3eb719043bcc5927f92ec
Su Zhang <me@zhang.su>
2015-08-11 22:14:58 -0500

    Add initial implementation of a naive BST

---
+++ src/binary_search_tree.cc
@@ -0,0 +1,52 @@
+#include "binary_search_tree.h"
+
+using std::endl;
+using std::ostream;
+
+BinarySearchTree::BinarySearchTree() : root_(nullptr) {}
+
+BinarySearchTree::~BinarySearchTree() {
+  delete root_;
+}
+
+BinarySearchTree* BinarySearchTree::add(int elem) {
+  return add(elem, &root_);
+}
+
+BinarySearchTree* BinarySearchTree::add(int elem, Node** pnode) {
+  Node* node = *pnode;
+  if (node == nullptr) {
+    *pnode = new Node(elem, nullptr, nullptr);
+    return this;
+  } else if (node->elem_ < elem) {
+    return add(elem, &(node->right_));
+  } else {
+    return add(elem, &(node->left_));
+  }
+}
+
+ostream& BinarySearchTree::output(ostream& stream, Node* node, int level) const {
+  if (node == nullptr) {
+    return stream;
+  }
+  output(stream, node->right_, level + 1);
+  for (int i = 0; i < level; ++i) {
+    stream << "  ";
+  }
+  stream << node->elem_ << endl;
+  output(stream, node->left_, level + 1);
+
+  return stream;
+}
+
+BinarySearchTree::Node::Node(int elem, Node* left, Node* right)
+    : elem_(elem), left_(left), right_(right) {}
+
+BinarySearchTree::Node::~Node() {
+  delete left_;
+  delete right_;
+}
+
+ostream& operator <<(ostream& stream, const BinarySearchTree& tree) {
+  return tree.output(stream, tree.root_, 0);
+};
